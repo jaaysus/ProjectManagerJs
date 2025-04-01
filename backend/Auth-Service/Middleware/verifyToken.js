7a9f2077770c932ecd3989f356eadd9e667874ed
ImaneLyachouti <liemyy70@gmail.com>
2025-04-01 19:57:52 +0100

    last auth-service

--- backend/Auth-Service/Middleware/verifyToken.js
+++ backend/Auth-Service/Middleware/verifyToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../Models/User');
 const verifyToken = (role = null) => {
   return async (req, res, next) => {
-    const token = req.headers.authorization?.split(" ")[1];//fixed token for front end
+      const token = req.headers['authorization'];
     if (!token) {
       return res.status(403).send('Le token est obligatoire pour l\'authentification');
     }
