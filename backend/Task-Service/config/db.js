092e257468d150d8e9b38fa24fca5f184209458e
[Alae Chentouf] <alaechentouf2@gmail.com>
2025-04-02 08:12:06 +0000

    last commit

--- backend/Task-Service/config/db.js
+++ backend/Task-Service/config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const connectDB=()=>{mongoose.connect('mongodb://localhost:27017/Task_DB')
+
+const connectDB=()=>{mongoose.connect('mongodb://mongo:27017/Task_DB')
 .then(()=>console.log('Your Connexion To MongoDB Is Successful (❁´◡`❁)'))
 .catch((error)=>console.error('Error connecting to mongodb',error));}
 const db=mongoose.connection;
